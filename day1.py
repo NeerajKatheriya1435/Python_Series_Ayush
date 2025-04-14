03324ce98f14031eeadb8e86d2b93eef9be99589
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-04-14 18:30:53 +0530

    Initial Commit

---
+++ day1.py
@@ -0,0 +1,26 @@
+# This is single line comment
+# this is the initioal code
+# print("Hello kaise ho")
+# print("Kaha j arhe ho")
+"""
+MultiLine Comments in python
+https://youtu.be/xA524YodYZs?si=h-OU0V-n8tw-uuAA
+FileNotFoundErrorlkgngvdfkngv
+lkdngvkdf
+;lvmdf;lvm
+lmvdf;lmv
+klvndfknv
+thjtfghjn
+a= int(input("Enter a number"))
+b= int(input("Enter a number"))
+print(a+b)
+
+"""
+# a= int(input('Enter a """number  \nvuiodshnvio\tudfhnoiv\ndfiovnoidf jviodfnv'))
+# b= int(input("Enter a               number"))
+# print(a+b)
+
+print("Ayush goes to company and 'met her friend")
+print('Ayush goes to company and \'met her friend')
+print("Ayush goes to company and \"met her friend")
+print("Ayush goes to company and \\\"met her friend")
