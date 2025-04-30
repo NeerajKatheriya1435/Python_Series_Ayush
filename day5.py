52cb0154fcd011d7f159dfb5f4063bb674a78461
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-04-30 18:40:05 +0530

    operators in python

---
+++ day5.py
@@ -0,0 +1,71 @@
+# print("This my simple calc")
+
+# num1=9
+# num2=3
+# Arithmetic operators
+
+# print("The sum of num1 and num2 is:",(num1+num2))
+# print("The sub of num1 and num2 is:",(num1-num2))
+# print("The mul of num1 and num2 is:",(num1*num2))
+# print("The div of num1 and num2 is:",(num1/num2))
+# print("The floor div of num1 and num2 is:",(num1//num2))
+# print("The modulo of num1 and num2 is:",(num1%num2))
+# print("The modulo of num1 and num2 is:",(num1**num2))
+
+
+
+# 10
+# 9 -->ciel
+# 8 ,8.9,8.4,8.999
+# 7 floor-->farsh
+# 6
+
+
+# 8.9--> floor value =8
+# 8.9--> ciel value =9
+
+
+# Comparison operator
+# num1=89
+# num2=67
+# num1=num2
+
+# print(num1==num2) 
+# print(num1!=num2) 
+# print(num1>=num2) 
+# print(num1>num2) 
+# print(num1<=num2) 
+# print(num1<num2) 
+
+num3=8
+num4=5
+
+# num4=num3
+
+# num4=87
+
+# num4**=2 # num4=num4+7
+
+# print(num4)
+
+# true and true -->true
+# 4>3 and 9>7  -->True
+
+# true and false -->false
+# 4>3 and 9>17  -->True
+
+# false or true or false--> true
+# 8>9 or 6>3 or 4>45
+
+# 8>9  -->false  true-->false
+
+# num2=8>9
+# print(not num2)
+
+print(15>8 and 7>6) 
+
+print(15>8 or 7>6)
+
+print(not 7>16)
+
+
