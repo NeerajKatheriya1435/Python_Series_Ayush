9da69a41df229d6d160ee549fbc51c45d578f68c
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-05-03 19:00:09 +0530

    some methods of string

---
+++ exercise1.py
@@ -0,0 +1,66 @@
+
+# question 4
+
+"""
+
+userInput=int(input("Enter the celcius temp to convert in foren: "))
+
+print(userInput,"celcius=",(userInput * 9/5) + 32,"Forenheit")
+
+"""
+# ammount=300  #-->(principal ammount --(p))
+# rate =5 #-->(rate -->(r))
+# time=1 # -->(t)
+
+# formula=p*r*t/100
+
+# print("Simple Interest",(ammount*rate*time)/100)
+
+# ayushStr="You are good boy"
+# print(ayushStr,"This is me",sep=",,",end="This is end")
+
+# name="Ayush"
+# name[0]="B"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+
+# listAyush=["ayush","shivam","kumar"]
+# listAyush[0]="Dusra"
+# print(listAyush)
+
+# iterable--> read character by charecter
+# immutable--> can not be change
+
+# ayushStr="Durgesh"
+# for item in ayushStr:
+#     print(item)
+
+# fruit = "This is my string"
+# print("The length of mango is",len(fruit))
+    # "0123456789"
+# pie = "AyushKumar"
+# print(pie[0:6])
+# print(pie[0:5])
+# print(pie[3:8])
+
+# strAyush1="This IS MY string"
+# print(strAyush1.upper())
+# print(strAyush1.lower())
+# str2 = "         Ayush Spoon    ???????? !!!!!!   "
+# print(str2)
+# print(str2.rstrip("?,!, "))
+
+# str2 = "Silver Spoon"
+# print(str2.replace("Silver", "Ayush"))
+
+str2 = "Ilver,Spoon,aYUSH,NEERAJ shuivbam"
+# print(str2.split(","))  
+# print(str2.capitalize())
+str1 = "Welcome to the Console!!!"
+# print(str1.center(50))
+# print(str1.count("e"))
+# print(str1.endswith("!!"))
+print(str1.find("you"))
+
+
