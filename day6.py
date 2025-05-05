c17775cd4e9b1e2aec6af0ee169eca5ccd1d34e1
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-05-05 18:42:07 +0530

    Rest string method and if-else

---
+++ day6.py
@@ -0,0 +1,46 @@
+# str2 = "Silver Spoon"
+# print(str2.replace("o","sp"))
+
+# str1 = "ihei is HJBKJDSBKJ JCBSDJK hjvjhv"
+# capStr1 = str1.capitalize()
+# print(capStr1)
+
+# str2 = "Abracadabra"
+# countStr = str2.count("a")
+# print(countStr)
+  #    "0123456789"
+# str1 = "Welcome to the Console !!!"
+# print(str1.endswith("e"))
+
+# str1 = "He's name is Dan. He is an honest man."
+
+# print(str1.find("isgdfgd"))  # rerturn
+# print(str1.index("is34645"))  # rerturn
+
+# ayushStr="Yuahsheklo"
+# print(ayushStr.isalpha())
+
+# str1 = "hello world"
+# print(str1.islower())
+# print(str1.islower())
+
+# str1 = "We wish you a Merry \nChristmas"
+# print(str1.isprintable())
+
+# str1 = "          " #using Spacebar
+# print(str1.isspace())
+
+# str1 = "World Halth Organization"
+# print(str1.istitle())
+
+# str1 = "WORLD HEALTH SDD ORGANIZATION"
+# print(str1.isupper())
+
+# str1 = "Python is a Interpreted Language"
+# print(str1.startswith("RPython"))
+
+# str1 = "Python is a Interpreted Language"
+# print(str1.swapcase())
+
+str1 = "the lucifier morning star"
+print(str1.title())
---
+++ day7.py
@@ -0,0 +1,25 @@
+userInp=int(input("Enter your marks: "))
+
+# if condition:
+# # code to run if condition is True
+# else:
+# # code to run if condition is False
+
+# if userInp>=18:
+#     print("You can drive the car")
+
+# else:
+#     print("You can not drive the car")
+
+if userInp>90:
+    print("Excellent Marks")
+
+elif userInp>60:
+    print("good marks")
+
+elif userInp>40:
+    print("average marks")
+
+else:
+    print("You are fail")
+
