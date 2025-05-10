5d379219b9b2cb71f1b9c3d2292ec5283f73b4de
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-05-10 18:52:24 +0530

    Break and continue

---
+++ day10.py
@@ -0,0 +1,18 @@
+# inp1=int(input("Enter a number"))
+# i=1
+# while(i<11):
+#     print(i)
+#     i=i+1
+
+for i in range(1,11):
+    if(i==5):
+        print("ayush ko print nhi karmna haui")
+    # print("This is shyam")
+
+        #this block will not execute
+        # break
+        continue
+    print(i)
+
+print("End the Loop")
+
