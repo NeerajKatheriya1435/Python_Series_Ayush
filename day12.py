a026a9bf4a427f2911605c51102ffb1315a6624e
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-05-12 18:59:50 +0530

    Function explained

---
+++ day12.py
@@ -0,0 +1,21 @@
+
+# i=1
+# while(i<11):
+#     if(i==3 or i==8):
+#         i=i+1
+#         continue
+#     print(i)
+#     i=i+1
+
+# print("Control is here")
+
+def ayushFunc():
+    print("Hello ayush")
+
+def addTwoNum():
+    inp1=9
+    inp2=8
+    print("The sum is",(inp1+inp2))
+
+ayushFunc() # calling a function
+addTwoNum() # calling the function
---
+++ day13.py
@@ -0,0 +1,6 @@
+
+def greet(name):
+    print("Hello good Morning",name)
+
+# greet("Ayush")
+# greet("Neeraj")
